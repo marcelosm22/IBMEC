e2b507fcfce4beaa968eb43264f8a2207c8a2c4c
marcelosm22 <122774271+marcelosm22@users.noreply.github.com>
2023-10-03 17:07:11 -0300

    Add files via upload

---
+++ ap1.py
@@ -0,0 +1,10 @@
+print("Se x = 7 \n y = 8 \n temp = x + y \n x = y \n y = x \n temp = x + y \n y = temp + 4 \n DISPLAY (y) \n \n (A) 0 \n (B) 15 \n (C) 16 \n (D) 20 \n")
+
+resposta = input("Qual é a resposta? ")
+
+if resposta == "D" or resposta == "d":
+    print("Parabéns, você acertou!")
+else:
+    print("Que pena! Você errou!")
+
+
